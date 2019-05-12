793645fec954d67cbccb62a9c55f00702f9d01a5
Ronan Felipe <ronan.nascimento@sga.pucminas.br>
2019-05-12 20:30:19 -0300

    Django viewset for Ativos

--- bolsa/views.py
+++ bolsa/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+
+from .models import Ativos
+from .serializers import AtivosSerializer
 
 # Create your views here.
+
+
+class AtivoSet(viewsets.ModelViewSet):
+    queryset = Ativos.objects.all()
+    serializer_class = AtivosSerializer
