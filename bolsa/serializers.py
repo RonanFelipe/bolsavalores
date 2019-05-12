89b9eaa1ceab36e789cae92744346de012553755
Ronan Felipe <ronan.nascimento@sga.pucminas.br>
2019-05-12 20:26:48 -0300

    Serializer ativos

---
+++ bolsa/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from .models import Ativos
+
+
+class AtivosSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Ativos
+        fields = ('id', 'nome', 'codigo', 'descricao')
