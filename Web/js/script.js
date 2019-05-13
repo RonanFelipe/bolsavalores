d05ff0a40d76ba83bf5b5e60b12589c1bd60923f
Ronan Felipe <ronan.nascimento@sga.pucminas.br>
2019-05-13 01:12:26 -0300

    add qtd attribute

--- Web/js/script.js
+++ Web/js/script.js
@@ -10,13 +10,18 @@ function loadAtivos() {
                 divAtivos.classList.add('has-addons');
                 let firstSpan = document.createElement("span");
                 let secondSpan = document.createElement("span");
+                let thirdSpan = document.createElement("span");
                 firstSpan.classList.add('tag');
                 secondSpan.classList.add('tag');
                 secondSpan.classList.add('is-info');
+                thirdSpan.classList.add('tag');
+                thirdSpan.classList.add('is-warning');
                 firstSpan.innerHTML = ativos[i].nome;
                 secondSpan.innerHTML = "R$ " + ativos[i].preco;
+                thirdSpan.innerHTML = "Qtd " + ativos[i].quantidade;
                 divAtivos.appendChild(firstSpan);
                 divAtivos.appendChild(secondSpan);
+                divAtivos.appendChild(thirdSpan);
                 coluna.appendChild(divAtivos);
             }
         })
