a65650fd30bd42637d6ce4d7fb09d2b5d86a03b7
Ronan Felipe <ronan.nascimento@sga.pucminas.br>
2019-05-13 19:57:29 -0300

    compra & fake user serializer

--- bolsa/serializers.py
+++ bolsa/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Ativos
+from .models import Ativos, FakeUser, CompraAtivos
 
 
 class AtivosSerializer(serializers.ModelSerializer):
@@ -8,3 +8,22 @@ class AtivosSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ativos
         fields = ('id', 'nome', 'codigo', 'descricao', 'preco', 'quantidade')
+
+
+class FakeUserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = FakeUser
+        fields = ('id', 'name', 'nacionalidade')
+
+
+class CompraSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = CompraAtivos
+        fields = ('id', 'ativos', 'user')
+
+    def to_representation(self, instance):
+        response = super().to_representation(instance)
+        response['user'] = FakeUserSerializer(instance.user).data
+        response['ativos'] = AtivosSerializer(instance.ativos).data
