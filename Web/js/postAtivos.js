b25895371a57c6020bbf3827bd4cc64b6edd895d
Ronan Felipe <ronan.nascimento@sga.pucminas.br>
2019-05-17 22:37:21 -0300

    show all users

--- Web/js/postAtivos.js
+++ Web/js/postAtivos.js
@@ -4,8 +4,9 @@ function createAtivos() {
     let nome = document.getElementById("nomeAtivo").value;
     let codigo = document.getElementById("codigoAtivo").value;
     let descricao = document.getElementById("descricaoAtivo").value;
-    let quantidade = document.getElementById("qtdAtivo").value;
+    let usuario = document.getElementById("usuario").value;
     let preco = document.getElementById("precoAtivo").value;
+    let status = 0;
     fetch('http://127.0.0.1:8000/api/v1/ativos/', {
         method: 'post',
         headers: {
@@ -16,8 +17,9 @@ function createAtivos() {
             nome: nome,
             codigo: codigo,
             descricao: descricao,
-            quantidade: quantidade,
             preco: preco,
+            user: usuario,
+            status: status
         })
     }).then(res => res.json())
         .then(res => console.log(res));
